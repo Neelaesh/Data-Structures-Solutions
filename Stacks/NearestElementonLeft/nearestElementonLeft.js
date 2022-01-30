9cc06de2ddafbf97cf4c545cbcb12825ec89b0b7
Neelaesh <neelaesh006@gmail.com>
2022-01-30 17:48:56 +0530

    DS 1

---
+++ Stacks/NearestElementonLeft/nearestElementonLeft.js
@@ -0,0 +1,13 @@
+console.log("nearestElementonLeft");
+
+function nearestElementonLeft(arr) {
+    // Create an empty Stack
+    var stack = []
+    for(let i = 0; i<arr.length; i++) {
+        
+    }
+    
+}
+
+let arr = [ 1, 3, 0, 2, 5 ]
+console.log(nearestElementonLeft(arr))
